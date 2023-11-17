97f0c40b30bd72061eb5e0254c5c7f979d2e72a3
parteek kumar <digitalparteek@gmail.com>
2023-11-17 23:50:52 +0530

    Added final code

---
+++ modules/room/components/Room.tsx
@@ -0,0 +1,33 @@
+import { useRoom } from "@/common/recoil/room";
+
+import RoomContextProvider from "../context/Room.context";
+import Canvas from "./board/Canvas";
+import MousePosition from "./board/MousePosition";
+import MousesRenderer from "./board/MousesRenderer";
+import MoveImage from "./board/MoveImage";
+import SelectionBtns from "./board/SelectionBtns";
+import NameInput from "./NameInput";
+import ToolBar from "./toolbar/ToolBar";
+import UserList from "./UserList";
+
+const Room = () => {
+  const room = useRoom();
+
+  if (!room.id) return <NameInput />;
+
+  return (
+    <RoomContextProvider>
+      <div className="relative h-full w-full overflow-hidden">
+        <UserList />
+        <ToolBar />
+        <SelectionBtns />
+        <MoveImage />
+        <Canvas />
+        <MousePosition />
+        <MousesRenderer />
+      </div>
+    </RoomContextProvider>
+  );
+};
+
+export default Room;
