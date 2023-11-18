4360ca2b133077a1dd120f2a4b44d1964c4b3cae
parteek kumar <digitalparteek@gmail.com>
2023-11-18 17:31:08 +0530

    final*

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 
-import Home from "@/modules/home/components/Home";
 import CombinedComponent from "@/modules/room/components/Room";
 
 const HomePage: NextPage = () => {
