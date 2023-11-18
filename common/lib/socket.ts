93fb0f841d6faee7e3471537cfbab95ff65f061f
parteek kumar <digitalparteek@gmail.com>
2023-11-18 16:55:39 +0530

    socket url changes

--- common/lib/socket.ts
+++ common/lib/socket.ts
@@ -2,4 +2,6 @@ import { io, Socket } from "socket.io-client";
 
 console.log("Socket.ts file running");
 
-export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io();
+export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
+  "https://unstudio-canvas-ptef.vercel.app/"
+);
